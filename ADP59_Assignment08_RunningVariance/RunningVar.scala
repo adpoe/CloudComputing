984fd170d9ac2f0fe1484f061f663a4e2303dd35
Tony Poerio <tony@tonypoer.io>
2017-04-16 17:01:59 -0400

    Added examples for assignment08, and submitted

--- ADP59_Assignment08_RunningVariance/RunningVar.scala
+++ ADP59_Assignment08_RunningVariance/RunningVar.scala
@@ -1,4 +1,4 @@
-package com.sundogsoftware.spark
+// com.your.package.here
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
@@ -8,25 +8,26 @@ import org.apache.spark.SparkContext._
  * https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
  */
 // Must extend Serializable to avoid this error:
-//    17/04/16 16:25:16 ERROR Executor: Exception in task 0.0 in stage 0.0 (TID 0)
-//    java.io.NotSerializableException
-class VarianceAssignment extends Serializable { 
-  
+//    * 17/04/16 16:25:16 ERROR Executor: Exception in task 0.0 in stage 0.0 (TID 0)
+//    * java.io.NotSerializableException
+class VarianceAssignment extends java.io.Serializable {
+
   // declare all necessary variables
   var n: Double = 0.0      // total count of values we have seen
   var mean: Double = 0.0   // running mean
   var delta: Double = 0.0  // change for this iteration
   var delta2: Double = 0.0 // change for this iteration squared
   var M2: Double = 0.0     // a running delta*delta2
- 
-  // Compute initial variance for numbers 
- def this(data: Iterator[Double]) { 
-    this  // must add a 'this' to avoid error:  --> 'this' expected but identifier found.
-    data.foreach((this.add(_)))
- } 
-  
+
+  // Compute initial variance for numbers
+ def this(data: Iterator[Double]) {
+    this  // must add a 'this' to avoid error:
+          //     * " 'this' expected but identifier found. "
+    data.foreach(this.add(_))
+ }
+
  // Update variance for a single value
- def add(value: Double) { 
+ def add(value: Double) {
    // value = current number we are iterating over
    n += 1
    delta = value - mean
@@ -34,7 +35,7 @@ class VarianceAssignment extends Serializable {
    delta2 = value - mean
    M2 += delta*delta2
  }
- 
+
  // Merge another variance object, and update the variance
  def merge(other: VarianceAssignment): VarianceAssignment = {
    // merge values with another instance of this class...
@@ -45,7 +46,7 @@ class VarianceAssignment extends Serializable {
    other.M2 += M2
    return other : VarianceAssignment
  }
- 
+
  // a 'Get' method. Use this to return the running variance at any given time.
 def getVariance(): Double = {
   // calculate the variance --> M2/n
@@ -56,35 +57,36 @@ def getVariance(): Double = {
 
 // object to use as an entry point when running from Scala IDE
 object VarianceExample {
-  
+
   // main method, required in order to run the program from IDE
   def main(args: Array[String]) {
-    
+
     // create a new spark context and use local machine, all cores
     val sc = new SparkContext("local[*]", "RunningVariance")
-    
+
     // update code from last assignment to create 100 doubles,
     // in range [0 --> 100]
     val r = scala.util.Random
     val idx = 1 to 100
-    
+
     // and ensure that these values are parallelized in an RDD
     val myRdd = sc.parallelize(idx.map(i => r.nextDouble()*100))
 
     // use my new class to calculate the variance
     // all done by merging, in one pass
     val myRunningVar = myRdd
-      .mapPartitions(v=>Iterator(new VarianceAssignment(v))) 
+      .mapPartitions(v=>Iterator(new VarianceAssignment(v)))
       .reduce((a,b) => a.merge(b))
-    
+
     // use my get method to return the final variance
     val myVariance = myRunningVar.getVariance()
-    
+
     // for comparison, use Scala-Spark's built in variance method
-    val scalaVar = myRdd.variance() 
-    
+    val scalaVar = myRdd.variance()
+
     // and print both results to compare the values
     println(s"My Result: $myVariance ;; Scala's Result: $scalaVar")
- 
+
+
     }
 }
